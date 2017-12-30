fa129c4891fd0cb540498c9fa704e792b0a1d1a6
SercDem <sercantoupload@hotmail.com>
2017-12-31 01:02:30 +0200

    q

---
+++ ReactTesting/src/app/fourth.js
@@ -0,0 +1,22 @@
+var React=require('react');
+var createReactClass=require('create-react-class');
+import{Link}from 'react-router';
+var fourth=createReactClass({
+  render:function(){
+    return(
+      <div>
+<Link className="rr" to={'/'}>First Page </Link>
+<Link className="rr" to={'/second'}> Second Page </Link>
+<Link className="rr" to={'/third'}> Third Page </Link>
+<Link className="rr" to={'/fourth'}> Fourth Page </Link>
+<style>
+{"\.rr{padding:5px;Text-decoration:none;\}\
+"}
+</style>
+<h3>Fourth Page</h3>
+</div>
+    );
+}
+
+});
+module.exports=fourth;
---
+++ ReactTesting/src/app/third.js
@@ -0,0 +1,22 @@
+var React=require('react');
+var createReactClass=require('create-react-class');
+import{Link}from 'react-router';
+var third=createReactClass({
+  render:function(){
+    return(
+      <div>
+<Link className="rr" to={'/'}>First Page </Link>
+<Link className="rr" to={'/second'}> Second Page </Link>
+<Link className="rr" to={'/third'}> Third Page </Link>
+<Link className="rr" to={'/fourth'}> Fourth Page </Link>
+<style>
+{"\.rr{padding:5px;Text-decoration:none;\}\
+"}
+</style>
+<h3>Third Page</h3>
+</div>
+    );
+}
+
+});
+module.exports=third;
